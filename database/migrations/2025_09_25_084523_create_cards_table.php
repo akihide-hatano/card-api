6c36f715bb376eb668ff5b461ed8aac1e9fbe26f
akihide-hatano <a.h.19841202@gmail.com>
2025-09-25 18:01:02 +0900

    migrations/2025_09_25_084523_create_cards_tableを修正

--- database/migrations/2025_09_25_084523_create_cards_table.php
+++ database/migrations/2025_09_25_084523_create_cards_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
+            $table->string('title',100);
+            $table->text('description')->nullable();
+            $table->string('status', 20)->default('open'); // open | in_progress | done | archived
+            $table->date('due_date')->nullable();
+            $table->timestamp('archived_at')->nullable();
             $table->timestamps();
+            $table->index(['status', 'created_at']);
         });
     }
 
