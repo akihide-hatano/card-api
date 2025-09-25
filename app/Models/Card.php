4af49907dee6da9d057a77f62143fac9243bebbc
akihide-hatano <a.h.19841202@gmail.com>
2025-09-25 18:12:03 +0900

    Card.phpを更新しました

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminata\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory as FactoriesHasFactory;
 
 class Card extends Model
 {
-    //
+    use FactoriesHasFactory;
+
+    protected $fillable = ['title','description','status','due_date','archived_at'];
+
+    protected $casts = [
+        'due_date'    => 'date',
+        'archived_at' => 'datetime',
+    ];
 }
