6c5cd906d01fbe5e22644b158c46e1a3e98fbcb0
akihide-hatano <a.h.19841202@gmail.com>
2025-09-26 17:24:19 +0900

    CardApiTest.phpを修正。

--- tests/Feature/CardApiTest.php
+++ tests/Feature/CardApiTest.php
@@ -18,10 +18,26 @@ class CardApiTest extends TestCase
         Card::create(['title' => 'A']);
         Card::create(['title' => 'B']);
         Card::create(['title' => 'C']);
-        
+
         $res = $this->getJson('/api/v1/cards');
 
         $res->assertStatus(200)
             ->assertJsonStructure(['data', 'links', 'meta']);
     }
+
+    public function test_index_can_filter_by_q()
+{
+    Card::create(['title' => 'Laravel API Guide']);
+    Card::create(['title' => 'React API Frontend']);
+    Card::create(['title' => 'API Testing with PHPUnit']);
+
+    $res = $this->getJson('/api/v1/cards?q=React');
+
+    $res->assertStatus(200)
+        ->assertJsonPath('meta.total', 1)
+        ->assertDontSee('Laravel API Guide')
+        ->assertDontSee('API Testing with PHPUnit')
+        ->assertSee('React API Frontend');
+}
+
 }
