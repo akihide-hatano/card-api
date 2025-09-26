e89a44b16a570eff0b7a057bd3ce4d8f7823dbc0
akihide-hatano <a.h.19841202@gmail.com>
2025-09-26 13:25:30 +0900

    api.phpを修正

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,6 @@
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\CardController;
 
-    // Route::prefix('v1')->group(function() {
-    //         Route::apiResource('cards', CardController::class);
-    //     Route::post('cards/{card}/archive', [CardController::class, 'archive'])
-    //         ->name('cards.archive');
-    // });
-
     Route::prefix('v1')->group(function () {
     Route::apiResource('cards', CardController::class);
     Route::post('cards/{card}/archive', [CardController::class, 'archive'])
